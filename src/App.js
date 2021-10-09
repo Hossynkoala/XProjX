275070eb5c1b519fd7e307017423c42fe6c15ef9
hossyn assghary <hossynassghary@gmail.com>
2021-10-09 22:28:58 +0330

    Frist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    Grid,
+} from "@mui/material";
+import Feeds from "./Componnents/Feeds/Feeds";
+import AppBar from"./Componnents/AppBar/AppBar"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Grid>
+            <Grid item>
+                <AppBar/>
+            </Grid>
+            <Grid>
+                <Feeds/>
+            </Grid>
+
+        </Grid>
+
+
+    );
 }
 
+
 export default App;
+
+
+
+
